42b05fabd34dd0c6499b645268cf3d146bf6b0a3
jmilthedude <jmiltheman@gmail.com>
2021-06-24 21:37:52 -0500

    added tab completion

--- src/main/java/net/thedudemc/endure/command/GetCommand.java
+++ src/main/java/net/thedudemc/endure/command/GetCommand.java
@@ -4,6 +4,11 @@ import net.thedudemc.endure.init.EndureItems;
 import net.thedudemc.endure.item.EndureItem;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.util.StringUtil;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class GetCommand extends Command {
     @Override
@@ -13,11 +18,26 @@ public class GetCommand extends Command {
 
     @Override
     public void execute(CommandSender sender, String[] args) {
-        Player p = (Player) sender;
-        String id = args[0];
-        EndureItem item = EndureItems.getItemById(id);
-        if (item != null) {
-            p.getInventory().addItem(item.getItemStack());
+        if(args.length == 1) {
+            Player p = (Player) sender;
+            String id = args[0];
+            EndureItem item = EndureItems.getItemById(id);
+            if (item != null) {
+                p.getInventory().addItem(item.getItemStack());
+            }
         }
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, org.bukkit.command.Command command, String alias, String[] args) {
+        if(args.length == 1) {
+            return StringUtil.copyPartialMatches(args[0], new ArrayList<String>() {
+                        {
+                            this.addAll(EndureItems.REGISTRY.keySet());
+                        }
+                    },
+                    new ArrayList<>(EndureItems.REGISTRY.size()));
+        }
+        return super.onTabComplete(sender, command, alias, args);
     }
 }
